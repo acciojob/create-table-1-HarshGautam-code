b80b6e14ba748f363c8b80b59532d54cb292a3f6
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-03-23 14:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function insert_Row() {
     //Write your code here
+
+	function insert_Row() {
+    // get table
+    let table = document.getElementById("sampleTable");
+
+    // insert new row at top (index 0)
+    let newRow = table.insertRow(0);
+
+    // create two cells
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    // add text
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+}
   
   
 }
